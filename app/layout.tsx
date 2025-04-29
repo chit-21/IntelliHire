84b44ec3a5f461d7f0203ee6a83eabeaa9be5b15
chit-21 <schitranshu040@gmail.com>
2025-04-30 00:05:57 +0530

    implement auth and storage

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Mona_Sans} from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const monasans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "IntelliHire",
+  description: "An AI powered  platform for mock interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monasans.className} antialiased pattern`}
+      >
+        {children}
+
+        <Toaster/>
+      </body>
+    </html>
+  );
+}
---
+++ firebase/client.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDPsiK4ehET39b7sydFFN2mJMmIDCA1qjQ",
+  authDomain: "intellihire-dfc73.firebaseapp.com",
+  projectId: "intellihire-dfc73",
+  storageBucket: "intellihire-dfc73.firebasestorage.app",
+  messagingSenderId: "163250090496",
+  appId: "1:163250090496:web:cea91b7a7bf983bd461ee5",
+  measurementId: "G-18WP3DD8GV"
+};
+
+// Initialize Firebase
+const app = !getApps.length?initializeApp(firebaseConfig):getApp();
+
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
